d3f0ce53bd71732d0e9258689d712e018bfe7e13
Aparna0597 <96976147+AparnaNidhi@users.noreply.github.com>
2022-04-12 16:30:46 +0530

    commit

--- src/model/authordata.js
+++ src/model/authordata.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 //database connection
-mongoose.connect('mongodb://localhost:27017/library');
+mongoose.connect('mongodb+srv://userone:userone@ictakfiles.ej6hq.mongodb.net/LIBRARYAPP?retryWrites=true&w=majority');
 
 //schema definition
 const schema=mongoose.Schema;
